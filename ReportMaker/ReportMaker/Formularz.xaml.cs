f104c0e799a85cc93918250c28188eae13c6b80f
RozbitekNawybrzezuKlatkiSchodowej <34235556+RozbitekNawybrzezuKlatkiSchodowej@users.noreply.github.com>
2017-12-04 13:09:06 +0100

    Aktualizacja okna

--- ReportMaker/ReportMaker/Formularz.xaml.cs
+++ ReportMaker/ReportMaker/Formularz.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ReportMaker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Formularz.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Formularz : Window
     {
-        public MainWindow()
+        public Formularz()
         {
             InitializeComponent();
-        }
-
-        private void NowyButton_Click(object sender, RoutedEventArgs e)
-        {
-            Formularz f = new Formularz();
-            f.Show();
         }
     }
 }
