d0ad70cd1a21d21aee62762c1e31294ef5a349ea
grobocop12 <34235543+grobocop12@users.noreply.github.com>
2017-12-04 13:51:47 +0100

    ulepszonko

--- ReportMaker/ReportMaker/Funkcja.cs
+++ ReportMaker/ReportMaker/Funkcja.cs
@@ -10,38 +10,39 @@ namespace ReportMaker
     {
         public double Suma(double x, double y)
         {
-            double wynik;
-            return wynik = x + y;
+            return  x + y;
         }
 
         public double Roznica(double x, double y)
         {
-         
             return  x - y;
         }
 
         public double Iloczyn(double x, double y)
         {
-          
             return  x * y;
         }
+
         public double Iloraz(double x, double y)
         {
-            
             return   x / y;
         }
+
         public double Logarytm_Naturalny(double x)
         {
             return Math.Log(x);
         }
+
         public double Exp(double x)
         {
             return Math.Exp(x);
         }
+
         public double Potegowanie(double x,double n)
         {
             return Math.Pow(x, n);
         }
+
     }
 
 
