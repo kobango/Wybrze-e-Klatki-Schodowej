24cb50ebf855f9bb319abf07ec8353b632ab113c
grobocop12 <34235543+grobocop12@users.noreply.github.com>
2017-12-04 13:44:00 +0100

    pobieram

---
+++ ReportMaker/ReportMaker/Funkcja.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportMaker
+{
+    public class Funkcja
+    {
+        public double Suma(double x, double y)
+        {
+            double wynik;
+            return wynik = x + y;
+        }
+
+        public double Roznica(double x, double y)
+        {
+         
+            return  x - y;
+        }
+
+        public double Iloczyn(double x, double y)
+        {
+          
+            return  x * y;
+        }
+        public double Iloraz(double x, double y)
+        {
+            
+            return   x / y;
+        }
+        public double Logarytm_Naturalny(double x)
+        {
+            return Math.Log(x);
+        }
+        public double Exp(double x)
+        {
+            return Math.Exp(x);
+        }
+    }
+
+
+
+
+    
+
+
+    
+}
